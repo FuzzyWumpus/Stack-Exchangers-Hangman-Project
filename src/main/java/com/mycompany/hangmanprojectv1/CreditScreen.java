1e606849608b9a8aff3e3044603fca76693010f9
scursaro <scursaro@cpp.edu>
2022-10-16 23:43:29 -0700

    Fixed ESC and F1 bug

--- src/main/java/com/mycompany/hangmanprojectv1/CreditScreen.java
+++ src/main/java/com/mycompany/hangmanprojectv1/CreditScreen.java
@@ -4,6 +4,14 @@
  */
 package com.mycompany.hangmanprojectv1;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
+import javax.swing.WindowConstants;
+
 /**
  *
  * @author ethan
@@ -31,6 +39,33 @@ public class CreditScreen extends javax.swing.JFrame {
         //setSize(600,400);
         //jPanel1.setSize(600,400);
         setLocationRelativeTo(null);
+         getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "Cancel"); //$NON-NLS-1$
+        getRootPane().getActionMap().put("Cancel", new AbstractAction(){ //$NON-NLS-1$
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        });
+        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+            KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0), "Pop"); //$NON-NLS-1$
+        getRootPane().getActionMap().put("Pop", new AbstractAction(){ //$NON-NLS-1$
+            public void actionPerformed(ActionEvent z)
+            {
+                popUp obj2 = new popUp();
+                obj2.setVisible(true);
+                
+            }
+        });
+        
+        // on close window the close method is called
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) 
+            {
+                System.exit(0);
+            }
+        });
     }
 
     /**
